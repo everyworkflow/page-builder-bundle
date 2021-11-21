65e72c90cf249c1ba67f0c7fe2d456dd2b1d6e2e
Suson Waiba <susonwaiba@gmail.com>
2021-11-21 05:02:35 +0000

    Added ?ExtensionInterface

--- EveryWorkflowPageBuilderBundle.php
+++ EveryWorkflowPageBuilderBundle.php
@@ -9,11 +9,12 @@ declare(strict_types=1);
 namespace EveryWorkflow\PageBuilderBundle;
 
 use EveryWorkflow\PageBuilderBundle\DependencyInjection\PageBuilderExtension;
+use Symfony\Component\DependencyInjection\Extension\ExtensionInterface;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class EveryWorkflowPageBuilderBundle extends Bundle
 {
-    public function getContainerExtension()
+    public function getContainerExtension(): ?ExtensionInterface
     {
         return new PageBuilderExtension();
     }
